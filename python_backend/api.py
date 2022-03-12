bd6520fe2dc324ae8af3d48f1fa61b6a84546967
ntut-xuan <t109590031@ntut.org.tw>
2022-03-13 03:34:17 +0800

    update(2022/03/13)

--- python_backend/api.py
+++ python_backend/api.py
@@ -15,30 +15,28 @@ def NearByStation():
 
     key = open("../gcp_secret", "r").read()
 
-    bus_station_data = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?&language=zh-TW&location=%f,%f&radius=%d&types=bus_station&key=%s" % (lat, lng, radius, key))
-    subway_station_data = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?&language=zh-TW&location=%f,%f&radius=%d&types=subway_station&key=%s" % (lat, lng, radius, key))
-
-    bus_station_set = set()
-    result = {}
+    bus_station_data = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?&language=zh-TW&location=%f,%f&rankby=distance&types=transit_station&key=%s" % (lat, lng, key))
+    bus_station_set = []
+    result = []
 
     bus_station_json_object = json.loads(bus_station_data.text)
 
     for object in bus_station_json_object["results"]:
         name = object["name"]
-        bus_station_set.add(name)
+        if name in bus_station_set:
+            continue
+        bus_station_set.append(name)
 
     for name in bus_station_set:
         if name not in bus_data_by_name:
             continue
-        if name not in result:
-            result[name] = []
         for stationID in bus_data_by_name[name].keys():
             stops = []
             for stop in bus_data_by_name[name][stationID]["stops"]:
                 stops.append(stop["RouteName"]["Zh_tw"])
-            result[name].append({"stationID": stationID, "lat": bus_data_by_name[name][stationID]["lat"], "lng": bus_data_by_name[name][stationID]["lng"], "stops": stops})
+            result.append({"name": name, "stationID": stationID, "lat": bus_data_by_name[name][stationID]["lat"], "lng": bus_data_by_name[name][stationID]["lng"], "stops": stops})
 
-    return result
+    return {"result": result}
     
 
 bus_stop_api_data = open("./static/bus_stop.json", "r", encoding="utf8").read()
